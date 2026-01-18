074e9e49d08beb71fd92e22f92809ceea208ee48
Riwaj-Pokhrel <riwajpokhrel99@gmail.com>
2026-01-18 14:53:43 +0545

    Day 1

---
+++ ch2 Strings/main.js
@@ -0,0 +1,13 @@
+const myVariable='Mathematics';
+console.log(myVariable.length)
+console.log("Every good boy does fine.".length)
+console.log(myVariable.charAt(0))
+console.log(myVariable.indexOf("ati"))
+console.log(myVariable.lastIndexOf('at'))
+console.log(myVariable.slice(5,8));
+console.log(myVariable.toUpperCase());
+console.log(myVariable.toLowerCase());
+console.log(myVariable.includes('ath'));
+console.log(myVariable.split('e'));
+console.log(myVariable.split(''));
+console.log("Every good boy does fine.".split(' '))
---
+++ ch3 numbers/main.js
@@ -0,0 +1,20 @@
+const myNumber=42;
+
+console.log(myNumber);
+const myFloat=42.00;
+console.log(myFloat);
+const myFloat2=42.04;
+console.log(myFloat2);
+console.log(myFloat==myNumber);
+myString='42.123abc';
+console.log(myString+5)
+console.log(Number(myString)+5)
+console.log(Number('Riwaj'))
+console.log(Number(false))
+console.log(Number.isInteger(myString))
+console.log(Number.parseFloat(myFloat))
+console.log(Number.parseFloat(myString))
+console.log(Number.parseFloat(myString).toFixed(2))
+console.log(Number.parseInt(myFloat))
+console.log(Number.parseInt(myString))
+console.log(typeof myFloat.toString())
